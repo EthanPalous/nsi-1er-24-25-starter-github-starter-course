b6d51cbab4c7b7baef507b6ee159083bbaaef003
Gianni973 <dolcegianni6@gmail.com>
2024-09-24 14:36:10 +0000

    update

--- exercice_4.py
+++ exercice_4.py
@@ -7,6 +7,7 @@ print(f"Périmètre du carré: {P}")
 print(f"Aire du carré: {A}")
 print(f"L'aire est-elle supérieure à 5 ? {b}")
 
-def perrimetre(x):
-    4 * c
+def perimetre(x):
+    return x * 4
 
+print("Périmètre du carré:", perimetre(5))
