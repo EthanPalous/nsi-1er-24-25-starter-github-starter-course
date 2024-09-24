250b6afb19edd401110e3dc16b26387190c34a10
Gianni973 <dolcegianni6@gmail.com>
2024-09-24 14:51:00 +0000

    update

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -6,12 +6,12 @@ b = A > 5
 print(f"Perimetre du carre: {P}")
 print(f" Aire du carre: {A}")
 print(f"L'aire est-elle superior a 5? {b}")
-
-def perimetre(c):
-    P = 4*c
+x = 5
+def perimetre(x):
+    P = 4*x
     return P
-def surface(c):
-    A = c **2
+def surface(x):
+    A = x **2
     return A
-print("le perimetre est:",perimetre(c))
-print("L'air est:", surface(c))
+print("le perimetre est:",perimetre(x))
+print("L'air est:", surface(x))
