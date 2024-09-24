917e7f979b55e7ddbdbf192deeabe23df3c61eda
Gianni973 <dolcegianni6@gmail.com>
2024-09-24 14:47:15 +0000

    update

--- exercice_4.py
+++ exercice_4.py
@@ -1,16 +1,15 @@
 c = 6
-P = 4 * c  
-A = c ** 2  
-b = A > 5
+P = 4 * c
+A = c ** 2
+b = A > 5 
 
-print(f"Périmètre du carré: {P}")
-print(f"Aire du carré: {A}")
-print(f"L'aire est-elle supérieure à 5 ? {b}")
+print(f"Perimetre du carre: {P}")
+print(f" Aire du carre: {A}")
+print(f"L'aire est-elle superior a 5? {b}")
 
 def perimetre(c):
     P = 4*c
     return P
-
 def surface(c):
     A = c **2
     return A
